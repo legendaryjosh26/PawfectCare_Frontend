3f890741c5e6aca4e718f5199938bc99018ef98a
josh-lazaro26 <joshualazaro262@gmail.com>
2026-01-16 08:54:05 +0800

    minor fixes

--- src/Components/PageTransition/UserLayout.jsx
+++ src/Components/PageTransition/UserLayout.jsx
@@ -1,5 +1,6 @@
 import { useLayoutEffect } from "react";
 import { Outlet, useLocation } from "react-router-dom";
+import TopNavUser from "../Navigation/TopNavUser";
 
 export default function UserLayout() {
   const location = useLocation();
