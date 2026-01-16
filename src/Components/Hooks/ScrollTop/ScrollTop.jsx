3e8e3c4dd5c9bb8e331156c8e37c7ea31210fb4e
josh-lazaro26 <joshualazaro262@gmail.com>
2026-01-16 08:42:58 +0800

    minor fixes

--- src/Components/Hooks/ScrollTop/ScrollTop.jsx
+++ src/Components/Hooks/ScrollTop/ScrollTop.jsx
@@ -1,12 +1,14 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-export const ScrollTop = () => {
+function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo({ top: 0, left: 0, behavior: "instant" });
+    window.scrollTo(0, 0);
   }, [pathname]);
 
   return null;
-};
+}
+
+export default ScrollToTop;
--- src/Components/PageTransition/UserLayout.jsx
+++ src/Components/PageTransition/UserLayout.jsx
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
 import TopNavUser from "../Navigation/TopNavUser";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 export default function UserLayout() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
     <>
       <TopNavUser />
